c440681179e263de5412e2b0d94983e22b0f9333
Muhammad Rehan <162461517+mrehankarim@users.noreply.github.com>
2026-05-12 23:18:16 +0500

    Update baseURL for local network testing

--- api/axios.ts
+++ api/axios.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 import { Platform } from 'react-native';
 
-// For Android Emulator use 10.0.2.2 instead of localhost
-// For iOS Simulator use localhost
-const baseURL = Platform.OS === 'android' ? 'http://10.0.2.2:8000/api/v1' : 'http://localhost:8000/api/v1';
+const baseURL = Platform.OS === 'android'
+  ? 'http://[IP_ADDRESS]/api/v1'
+  : 'http://localhost:8000/api/v1';
 
 export const api = axios.create({
   baseURL,
